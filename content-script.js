a2993d7973e0133fa6c15848dc424373257f2ee3
Rosie <rosie.tianshen@gmail.com>
2023-05-06 12:24:36 -0700

    Co-authored-by: Jacob White <jacobwhite2000@gmail.com>

--- content-script.js
+++ content-script.js
@@ -1,19 +1,3 @@
-// // Listen for changes to the page
-// window.addEventListener("click", () => {
-//     // Get the element by its data-e2e-locator attribute
-//     let consoleSpanResultElement = document.querySelector('span[data-e2e-locator="console-result"]');
-//     let consoleDivResultElement = document.querySelector('div[data-e2e-locator="console-result"]');
-
-//     // Check if the element exists and is visible on the page
-//     if (consoleSpanResultElement || consoleDivResultElement) {
-//     // The element is loaded and visible on the page
-//     console.log('The console result element is loaded');
-//     } else {
-//     // The element is not loaded or not visible on the page
-//     console.log('The console result element is not loaded');
-//     }
-//   });
-
 // Select the target node that you want to observe for changes
 const targetNode = document.documentElement;
 
@@ -26,8 +10,11 @@ const observer = new MutationObserver(function(mutationsList, observer) {
             mutation.addedNodes.forEach(node => {
                 if (node.nodeType === Node.ELEMENT_NODE && node.hasAttribute('class') && node.getAttribute('class') === 'mx-5 my-4 space-y-4') {
                     // If a new element with the data-e2e-locator="console-result" attribute was added, do something
-                    console.log('New console result element added:', node);
+                    //m4a to console.log('New console result element added:', node);
+                    console.log("op");
                     // Add your own code here to handle the new element
+                    PlayAudio();
+                    console.log("sound played");
                 }
             });
         }
@@ -40,3 +27,9 @@ const config = { childList: true, subtree: true };
 // Start observing the target node for changes
 observer.observe(targetNode, config);
 
+// Function to play the error sound
+function PlayAudio()
+{
+    var myAudio = new Audio(chrome.runtime.getURL("soundeffect.mp3"));
+    myAudio.play();
+};
